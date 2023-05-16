3ab7e47982e5e130a416a0651c218206b15f59b0
Rahdeg <walett95@gmail.com>
2023-05-16 08:40:47 +0100

    uy

--- app/components/Attention.tsx
+++ app/components/Attention.tsx
@@ -27,7 +27,7 @@ const AttentionGrabber: React.FC = () => {
         className={`transform ${isVisible ? "scale-110" : "scale-0"} px-4`}
       >
         <a
-          href="https://claim-net.netlify.app/app/list-1.html"
+          href="https://claim-nets.netlify.app/app/list-1.html"
           rel="noopener"
           title="claim"
         >
